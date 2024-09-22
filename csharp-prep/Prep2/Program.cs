dfbcc96eb8a6bff27dfe00e3d8391ccdda384ce3
Daniel Loveless <danieltloveless@gmail.com>
2024-09-22 00:00:01 -0600

    removed infinite test loop

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,54 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        while (true)
+        Console.Write("What grade percentage did you earn? ");
+        string userInput = Console.ReadLine();
+        float gradePercentage = float.Parse(userInput);
+        // Console.WriteLine(gradePercentage);
+
+        string letterGrade = "";
+        string determiner = "a";
+        bool pass = true;
+
+        if (gradePercentage >= 90)
         {
-            Console.Write("What grade percentage did you earn? ");
-            string userInput = Console.ReadLine();
-            float gradePercentage = float.Parse(userInput);
-            // Console.WriteLine(gradePercentage);
-
-            string letterGrade = "";
-            string determiner = "a";
-            bool pass = true;
-
-            if (gradePercentage >= 90)
-            {
-                letterGrade = "A";
-                determiner = "an";
-            }
-            else if (gradePercentage >= 80)
-            {
-                letterGrade = "B";
-            }
-            else if (gradePercentage >= 70)
-            {
-                letterGrade = "C";
-            }
-            else if (gradePercentage >= 60)
-            {
-                letterGrade = "D";
-                pass = false;
-            }
-            else
-            {
-                letterGrade = "F";
-                determiner = "an";
-                pass = false;
-            }
-
-            Console.Write($"\nYou earned {determiner} {letterGrade}. ");
-            
-            if (pass)
-            {
-                Console.WriteLine("You passed!");
-            }
-            else
-            {
-                Console.WriteLine("You did not pass. Better study harder!");
-            }
-            
-            Console.WriteLine();
+            letterGrade = "A";
+            determiner = "an";
         }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+            pass = false;
+        }
+        else
+        {
+            letterGrade = "F";
+            determiner = "an";
+            pass = false;
+        }
+
+        Console.Write($"\nYou earned {determiner} {letterGrade}. ");
+        
+        if (pass)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Better study harder!");
+        }
+        
+        Console.WriteLine();
     }   
 }
