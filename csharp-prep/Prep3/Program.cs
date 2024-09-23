726328a12eb218a5bcc4f1ffaa7b3a1e9f5f33e0
Daniel Loveless <danieltloveless@gmail.com>
2024-09-23 15:33:39 -0600

    Completed strectch challenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,29 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random numberGenerator = new Random();
-        int magicNumber = numberGenerator.Next(1, 101);
-        
-        // Console.Write("What is the magic number? ");
-        // int magicNumber = int.Parse(Console.ReadLine());
-
         while (true)
         {
-            Console.Write("What is your guess? ");
-            int guess = int.Parse(Console.ReadLine());
+            Console.Clear();
             
-            if (guess == magicNumber)
+            Random numberGenerator = new Random();
+            int magicNumber = numberGenerator.Next(1, 101);
+            
+            // Console.Write("What is the magic number? ");
+            // int magicNumber = int.Parse(Console.ReadLine());
+
+            int numGuesses = 0;
+            while (true)
             {
-                Console.WriteLine("You guessed it!");
+                numGuesses += 1;
+
+                Console.Write("What is your guess? ");
+                int guess = int.Parse(Console.ReadLine());
+                
+                if (guess == magicNumber)
+                {
+                    Console.WriteLine($"You guessed it in {numGuesses} tries!");
+                    break;
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                }
+            }
+
+            Console.Write("Would you like to play again? ");
+            string userResponse = Console.ReadLine().ToLower();
+
+            if (userResponse == "no" || userResponse == "n")
+            {
                 break;
-            }
-            else if (guess > magicNumber)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("Higher");
             }
         }
     }
